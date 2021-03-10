350e15848c881525719568c612d6e743518e3f95
Jyri <54314515+jYKKEEE@users.noreply.github.com>
2021-03-10 10:55:24 +0200

    before refactoring

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -27,33 +27,35 @@ const Settings = (props) => {
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* HAVAINNOT DROPDOWN */}
         <DropdownButton text='Lisää/poista havaintoja' labelText=''>
-          <div className='flex flex-col justify-center items-center pb-2'>
-            <div className='flex items-center'>
-              <select
-                name='notes'
-                className='text-black rounded w-52 mr-8'
-                placeholder='havainnot'
-              >
-                {notes.map((selection, index) =>
-                  selection === 'Valitse' ? (
-                    <option
-                      key={index}
-                      className=' shadow-xl text-black px-9'
-                      disabled
-                    >
-                      {selection}
-                    </option>
-                  ) : (
-                    <option
-                      key={index}
-                      className='text-base font-mono text-black'
-                    >
-                      {selection}
-                    </option>
-                  )
-                )}
-              </select>
-
+          <div className='flex flex-col justify-between items-start pb-3 text-lg font-mono'>
+            <div className='flex items-end text-white font-mono '>
+              <label htmlFor='notes' className='flex flex-col'>
+                Poista valinta:
+                <select
+                  name='notes'
+                  className='text-black rounded w-52 mr-12'
+                  placeholder='havainnot'
+                >
+                  {notes.map((selection, index) =>
+                    selection === 'Valitse' ? (
+                      <option
+                        key={index}
+                        className=' shadow-xl text-black px-9'
+                        disabled
+                      >
+                        {selection}
+                      </option>
+                    ) : (
+                      <option
+                        key={index}
+                        className='text-base font-mono text-black'
+                      >
+                        {selection}
+                      </option>
+                    )
+                  )}
+                </select>
+              </label>
               <TrashCan
                 className='text-red-700 hover:text-red-500'
                 onClick={() => {
@@ -61,15 +63,18 @@ const Settings = (props) => {
                 }}
               />
             </div>
-            <div className='pt-4 pb-3'>
-              <input
-                type='text'
-                name='note'
-                placeholder='Lisää havaintoja listalle:'
-                ref={register()}
-                required
-                className='text-black text-lg rounded font-mono w-52 pr-2'
-              />
+            <div className=' flex flex-row pb-3'>
+              <label htmlFor='note' className='flex flex-col text-white '>
+                Lisää uusi:
+                <input
+                  type='text'
+                  name='note'
+                  placeholder='Lisää havaintoja listalle:'
+                  ref={register()}
+                  required
+                  className='text-black text-lg rounded font-mono w-52 pr-2'
+                />
+              </label>
             </div>
             <Button primary type='submit'>
               Lisää
@@ -79,37 +84,43 @@ const Settings = (props) => {
 
         {/* SIJAINNIT DROPDOWN */}
         <DropdownButton text='Lisää/poista sijainteja' labelText=''>
-          <div className='flex flex-col justify-center items-center pb-2'>
-            <div className='flex justify-center items-center'>
-              <select
-                name='location'
-                className=' shadow-xl text-black rounded w-52 mr-8'
-                ref={register}
-              >
-                {animalLocations.map((animalLocation, index) => (
-                  <option
-                    key={index}
-                    className='text-base font-mono text-black'
-                  >
-                    {animalLocation}
-                  </option>
-                ))}
-              </select>
+          <div className='flex flex-col justify-between items-start pb-3 font-mono text-lg'>
+            <div className='flex items-end text-white '>
+              <label htmlFor='location' className='flex flex-col'>
+                Poista valinta:
+                <select
+                  name='location'
+                  className=' shadow-xl text-black rounded w-52 mr-12'
+                  ref={register}
+                >
+                  {animalLocations.map((animalLocation, index) => (
+                    <option
+                      key={index}
+                      className='text-base font-mono text-black'
+                    >
+                      {animalLocation}
+                    </option>
+                  ))}
+                </select>
+              </label>
               <TrashCan
                 onClick={() => {
                   confirm('poistetaanko');
                 }}
               />
             </div>
-            <div className='pt-4 pb-3'>
-              <input
-                type='text'
-                name='newAnimalLocation'
-                placeholder='Lisää sijainteja listalle:'
-                ref={register}
-                required
-                className='text-black text-lg rounded font-mono w-52'
-              />
+            <div className=' pb-3'>
+              <label htmlFor='note' className='flex flex-col text-white '>
+                Lisää uusi:
+                <input
+                  type='text'
+                  name='newAnimalLocation'
+                  placeholder='Lisää sijainteja listalle:'
+                  ref={register}
+                  required
+                  className='text-black text-lg rounded font-mono w-52'
+                />
+              </label>
             </div>
 
             <Button primary type='submit'>
@@ -121,4 +132,9 @@ const Settings = (props) => {
     </div>
   );
 };
+
+const AddRemove = (props) => {
+  return <div>Add Remove</div>;
+};
+
 export default Settings;

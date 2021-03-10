4f73f51dc97365732dc47a2bb48a741644f195ad
Jyri <54314515+jYKKEEE@users.noreply.github.com>
2021-03-10 11:38:12 +0200

    refactor settings

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -26,115 +26,77 @@ const Settings = (props) => {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* HAVAINNOT DROPDOWN */}
-        <DropdownButton text='Lisää/poista havaintoja' labelText=''>
-          <div className='flex flex-col justify-between items-start pb-3 text-lg font-mono'>
-            <div className='flex items-end text-white font-mono '>
-              <label htmlFor='notes' className='flex flex-col'>
-                Poista valinta:
-                <select
-                  name='notes'
-                  className='text-black rounded w-52 mr-12'
-                  placeholder='havainnot'
-                >
-                  {notes.map((selection, index) =>
-                    selection === 'Valitse' ? (
-                      <option
-                        key={index}
-                        className=' shadow-xl text-black px-9'
-                        disabled
-                      >
-                        {selection}
-                      </option>
-                    ) : (
-                      <option
-                        key={index}
-                        className='text-base font-mono text-black'
-                      >
-                        {selection}
-                      </option>
-                    )
-                  )}
-                </select>
-              </label>
-              <TrashCan
-                className='text-red-700 hover:text-red-500'
-                onClick={() => {
-                  confirm('poistetaanko');
-                }}
-              />
-            </div>
-            <div className=' flex flex-row pb-3'>
-              <label htmlFor='note' className='flex flex-col text-white '>
-                Lisää uusi:
-                <input
-                  type='text'
-                  name='note'
-                  placeholder='Lisää havaintoja listalle:'
-                  ref={register()}
-                  required
-                  className='text-black text-lg rounded font-mono w-52 pr-2'
-                />
-              </label>
-            </div>
-            <Button primary type='submit'>
-              Lisää
-            </Button>
-          </div>
-        </DropdownButton>
-
+        <AddRemove
+          list={notes}
+          text='havainto'
+          inputName='note'
+          selectName='notes'
+          register={register}
+        />
         {/* SIJAINNIT DROPDOWN */}
-        <DropdownButton text='Lisää/poista sijainteja' labelText=''>
-          <div className='flex flex-col justify-between items-start pb-3 font-mono text-lg'>
-            <div className='flex items-end text-white '>
-              <label htmlFor='location' className='flex flex-col'>
-                Poista valinta:
-                <select
-                  name='location'
-                  className=' shadow-xl text-black rounded w-52 mr-12'
-                  ref={register}
-                >
-                  {animalLocations.map((animalLocation, index) => (
-                    <option
-                      key={index}
-                      className='text-base font-mono text-black'
-                    >
-                      {animalLocation}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <TrashCan
-                onClick={() => {
-                  confirm('poistetaanko');
-                }}
-              />
-            </div>
-            <div className=' pb-3'>
-              <label htmlFor='note' className='flex flex-col text-white '>
-                Lisää uusi:
-                <input
-                  type='text'
-                  name='newAnimalLocation'
-                  placeholder='Lisää sijainteja listalle:'
-                  ref={register}
-                  required
-                  className='text-black text-lg rounded font-mono w-52'
-                />
-              </label>
-            </div>
-
-            <Button primary type='submit'>
-              Lisää
-            </Button>
-          </div>
-        </DropdownButton>
+        <AddRemove
+          list={animalLocations}
+          text='sijainti'
+          inputName='newAnimalLocation'
+          selectName='location'
+          register={register}
+        />
       </form>
     </div>
   );
 };
 
 const AddRemove = (props) => {
-  return <div>Add Remove</div>;
+  const { list, text, register, inputName, selectName } = props;
+  return (
+    <div>
+      <DropdownButton text={`Lisää/poista ${text}`} labelText=''>
+        <div className='flex flex-col justify-between items-start pb-3 font-mono text-lg'>
+          <div className='flex items-end text-white '>
+            <label htmlFor='location' className='flex flex-col'>
+              Poista valinta:
+              <select
+                name={selectName}
+                className=' shadow-xl text-black rounded w-52 mr-12'
+                ref={register}
+              >
+                {list.map((obj, index) => (
+                  <option
+                    key={index}
+                    className='text-base font-mono text-black'
+                  >
+                    {obj}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <TrashCan
+              onClick={() => {
+                confirm('poistetaanko');
+              }}
+            />
+          </div>
+          <div className=' pb-3'>
+            <label htmlFor='note' className='flex flex-col text-white '>
+              Lisää uusi {text}:
+              <input
+                type='text'
+                name={inputName}
+                placeholder={`Lisää ${text} listalle`}
+                ref={register}
+                required
+                className='text-black text-lg rounded font-mono w-52'
+              />
+            </label>
+          </div>
+
+          <Button primary type='submit'>
+            Lisää
+          </Button>
+        </div>
+      </DropdownButton>
+    </div>
+  );
 };
 
 export default Settings;
